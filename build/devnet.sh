06caa9717937b10afc82d5964771a0f43bde3f6e
Jianrong <wjrjerome@gmail.com>
2021-08-14 20:21:25 +1000

    working version

--- build/devnet.sh
+++ build/devnet.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-
-echo "--------------------------1"
-echo "${SSH_KEY}"
-echo "--------------------------2"
-
 echo "${SSH_KEY}" > tem.pem && chmod 400 tem.pem
 # Trigger upgrade to a specific commit
 cat tem.pem
 
+ssh -o "StrictHostKeyChecking no" -i tem.pem $SSH_USER_AND_ADDRESS -p $DEVNET_PORT 'cd XinFin/Local_DPoS_Setup && ls'
 
 # clean up
 rm -rf tem.pem

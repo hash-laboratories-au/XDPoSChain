2361843605bf5c9a1af0aef702bc8e9ef0dd6447
Jianrong <wjrjerome@gmail.com>
2021-08-14 20:35:14 +1000

    remove the cat

--- build/devnet.sh
+++ build/devnet.sh
@@ -2,7 +2,6 @@
 
 echo "${SSH_KEY}" > tem.pem && chmod 400 tem.pem
 # Trigger upgrade to a specific commit
-cat tem.pem
 
 ssh -o "StrictHostKeyChecking no" -i tem.pem $SSH_USER_AND_ADDRESS -p $DEVNET_PORT 'cd XinFin/Local_DPoS_Setup && ls'
 

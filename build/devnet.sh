4545f0bad5d13a2968c691c522353cffbe4733ea
Jianrong <wjrjerome@gmail.com>
2021-08-14 18:01:03 +1000

    test token

--- build/devnet.sh
+++ build/devnet.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "${SSH_PEM}"
+echo "${ABC}"
 echo "${SSH_PEM}" > tem.pem && chmod 400 tem.pem
 # Trigger upgrade to a specific commit
 ssh -o "StrictHostKeyChecking no" -i tem.pem root@45.77.243.22 -p $DEVNET_PORT 'cd XinFin/Local_DPoS_Setup && ls'

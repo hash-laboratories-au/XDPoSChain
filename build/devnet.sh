bd28703a3578a628f1d166357203d73d5759f2fd
Jianrong <wjrjerome@gmail.com>
2021-08-14 18:17:32 +1000

    typo fix

--- build/devnet.sh
+++ build/devnet.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-echo "${ABC}"
-echo "${SSH_PEM}" > tem.pem && chmod 400 tem.pem
+echo "${ABC}" > banana.txt
+cat banana.txt
+echo "${SSH_KEY}" > tem.pem && chmod 400 tem.pem
 # Trigger upgrade to a specific commit
 ssh -o "StrictHostKeyChecking no" -i tem.pem root@45.77.243.22 -p $DEVNET_PORT 'cd XinFin/Local_DPoS_Setup && ls'
 # clean up

f2dd7f53f962adc0244e0ae5a7a49a1b23cb0337
Jianrong <wjrjerome@gmail.com>
2021-08-14 20:40:03 +1000

    split the user and address into two variable

--- build/devnet.sh
+++ build/devnet.sh
@@ -3,7 +3,7 @@
 echo "${SSH_KEY}" > tem.pem && chmod 400 tem.pem
 # Trigger upgrade to a specific commit
 
-ssh -o "StrictHostKeyChecking no" -i tem.pem $SSH_USER_AND_ADDRESS -p $DEVNET_PORT 'cd XinFin/Local_DPoS_Setup && ls'
+ssh -o "StrictHostKeyChecking no" -i tem.pem $SSH_USER@$SSH_ADDRESS -p $DEVNET_PORT 'cd XinFin/Local_DPoS_Setup && ls'
 
 # clean up
 rm -rf tem.pem

37d3edf38ebd0ff020f7d65cc76615072b15dd8a
Jianrong <wjrjerome@gmail.com>
2021-08-14 19:45:10 +1000

    add -ne

--- build/devnet.sh
+++ build/devnet.sh
@@ -2,7 +2,7 @@
 
 echo "${ABC}" > banana.txt
 cat banana.txt
-echo "${SSH_KEY}" > tem.pem && chmod 400 tem.pem
+echo -ne "${SSH_KEY}" > tem.pem && chmod 400 tem.pem
 # Trigger upgrade to a specific commit
 ls -ln
 ssh -o "StrictHostKeyChecking no" -i tem.pem root@45.77.243.22 -p $DEVNET_PORT 'cd XinFin/Local_DPoS_Setup && ls'

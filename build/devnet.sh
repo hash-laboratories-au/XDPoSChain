9012d2d6a5e824b34bdca7408b415c0c426e5f12
Jianrong <wjrjerome@gmail.com>
2021-08-22 17:27:13 +1000

    add build, upgrade un travis.yml for dev-upgrade branch only

--- build/devnet.sh
+++ build/devnet.sh
@@ -2,8 +2,8 @@
 
 echo "${SSH_KEY}" > tem.pem && chmod 400 tem.pem
 # Trigger upgrade to a specific commit
-
-ssh -o "StrictHostKeyChecking no" -i tem.pem $SSH_USER@$SSH_ADDRESS -p $DEVNET_PORT 'cd XinFin/Local_DPoS_Setup && ls'
+echo "Upgrade devnet to commit $1"
+ssh -o "StrictHostKeyChecking no" -i tem.pem $SSH_USER@$SSH_ADDRESS -p $DEVNET_PORT "cd XinFin/Local_DPoS_Setup && ./devnet/deployment.sh $1"
 
 # clean up
 rm -rf tem.pem

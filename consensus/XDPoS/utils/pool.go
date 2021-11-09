1ed524c48d6d39fc2c736605f16b99b804ff75a4
Gerui Wang <wgr523@gmail.com>
2021-11-09 17:37:16 +0800

    refine comment

--- consensus/XDPoS/utils/pool.go
+++ consensus/XDPoS/utils/pool.go
@@ -14,41 +14,41 @@ type Pool struct {
 	OnThresholdFn func([]PoolObj) error
 }
 
-func NewPool(certSize int) *Pool {
+func NewPool(threshold int) *Pool {
 	return &Pool{
 		objList:   make(map[string]map[common.Hash]PoolObj),
-		threshold: certSize,
+		threshold: threshold,
 	}
 }
 
-func (pool *Pool) Add(obj PoolObj) error {
-	objListKeyed, ok := pool.objList[obj.PoolKey()]
+func (p *Pool) Add(obj PoolObj) error {
+	objListKeyed, ok := p.objList[obj.PoolKey()]
 	if !ok {
-		pool.objList[obj.PoolKey()] = make(map[common.Hash]PoolObj)
-		objListKeyed = pool.objList[obj.PoolKey()]
+		p.objList[obj.PoolKey()] = make(map[common.Hash]PoolObj)
+		objListKeyed = p.objList[obj.PoolKey()]
 	}
 	objListKeyed[obj.Hash()] = obj
-	if len(objListKeyed) >= pool.threshold && pool.OnThresholdFn != nil {
-		objs := make([]PoolObj, pool.threshold)
+	if len(objListKeyed) >= p.threshold && p.OnThresholdFn != nil {
+		objs := make([]PoolObj, p.threshold)
 		i := 0
 		for _, t := range objListKeyed {
 			objs[i] = t
 			i += 1
-			if i == pool.threshold {
+			if i == p.threshold {
 				break
 			}
 		}
-		pool.Clear()
-		return pool.OnThresholdFn(objs)
+		p.Clear()
+		return p.OnThresholdFn(objs)
 	} else {
 		return nil
 	}
 }
 
-func (pool *Pool) Clear() {
-	pool.objList = make(map[string]map[common.Hash]PoolObj)
+func (p *Pool) Clear() {
+	p.objList = make(map[string]map[common.Hash]PoolObj)
 }
 
-func (pool *Pool) SetThreshold(t int) {
-	pool.threshold = t
+func (p *Pool) SetThreshold(t int) {
+	p.threshold = t
 }

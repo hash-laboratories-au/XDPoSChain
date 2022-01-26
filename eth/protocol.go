63038b3bcca904b3d70075834c7708b75b96b82b
Liam Lai <liam.icheng.lai@gmail.com>
2022-01-26 13:05:47 +0300

    add xdpos2 protocol

--- eth/protocol.go
+++ eth/protocol.go
@@ -30,18 +30,19 @@ import (
 
 // Constants to match up protocol versions and messages
 const (
-	eth62 = 62
-	eth63 = 63
+	eth62  = 62
+	eth63  = 63
+	xdpos2 = 100
 )
 
 // Official short name of the protocol used during capability negotiation.
 var ProtocolName = "eth"
 
 // Supported versions of the eth protocol (first is primary).
-var ProtocolVersions = []uint{eth63, eth62}
+var ProtocolVersions = []uint{xdpos2, eth63, eth62}
 
 // Number of implemented message corresponding to different protocol versions.
-var ProtocolLengths = []uint64{227, 8}
+var ProtocolLengths = []uint64{227, 17, 8}
 
 const ProtocolMaxMsgSize = 10 * 1024 * 1024 // Maximum cap on the size of a protocol message
 

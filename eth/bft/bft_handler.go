3792dbe3eb501a39dac0cdd049b37a82f06eed1e
Liam Lai <liam.icheng.lai@gmail.com>
2022-02-19 23:29:19 +0300

    update some hash log

--- eth/bft/bft_handler.go
+++ eth/bft/bft_handler.go
@@ -104,7 +104,7 @@ func (b *Bfter) Vote(vote *utils.Vote) error {
 	return nil
 }
 func (b *Bfter) Timeout(timeout *utils.Timeout) error {
-	log.Info("Receive Timeout", "timeout", timeout)
+	log.Info("Receive Timeout", "hash", timeout.Hash(), "round", timeout.Round)
 	if exist, _ := b.knownTimeouts.ContainsOrAdd(timeout.Hash(), true); exist {
 		log.Trace("Discarded Timeout, known Timeout", "Signature", timeout.Signature, "hash", timeout.Hash(), "round", timeout.Round)
 		return nil
@@ -128,7 +128,7 @@ func (b *Bfter) Timeout(timeout *utils.Timeout) error {
 	return nil
 }
 func (b *Bfter) SyncInfo(syncInfo *utils.SyncInfo) error {
-	log.Info("Receive SyncInfo", "syncInfo", syncInfo)
+	log.Info("Receive SyncInfo", "hash", syncInfo.Hash(), "HighQC Round", syncInfo.HighestQuorumCert.ProposedBlockInfo.Round, "HighTC Round", syncInfo.HighestTimeoutCert.Round)
 	if exist, _ := b.knownSyncInfos.ContainsOrAdd(syncInfo.Hash(), true); exist {
 		log.Trace("Discarded SyncInfo, known SyncInfo", "hash", syncInfo.Hash())
 		return nil

e19b78efe2832f62179ce4718b3442e41aabe755
Liam Lai <liam.icheng.lai@gmail.com>
2022-02-19 23:14:14 +0300

    update wait period for easy debug

--- params/config.go
+++ params/config.go
@@ -51,7 +51,7 @@ var (
 		SwitchBlock:           big.NewInt(7218000),
 		TimeoutWorkerDuration: 50,
 		CertThreshold:         2,
-		WaitPeriod:            2,
+		WaitPeriod:            10,
 		MinePeriod:            10,
 	}
 

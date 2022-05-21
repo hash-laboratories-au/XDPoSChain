47b16e0cb9b3c7224048a70bc456ba23dae32ca8
Liam Lai <liam.icheng.lai@gmail.com>
2022-05-20 21:36:50 -0500

    refactor and improve log

--- eth/bft/bft_handler.go
+++ eth/bft/bft_handler.go
@@ -78,7 +78,7 @@ func (b *Bfter) Vote(vote *utils.Vote) error {
 		err = b.consensus.voteHandler(b.blockChainReader, vote)
 		if err != nil {
 			if _, ok := err.(*utils.ErrIncomingMessageRoundTooFarFromCurrentRound); ok {
-				log.Warn("vote round not equal", "error", err, "vote", vote.Hash())
+				log.Debug("vote round not equal", "error", err, "vote", vote.Hash())
 				return err
 			}
 			log.Error("handle BFT Vote", "error", err)
@@ -102,7 +102,7 @@ func (b *Bfter) Timeout(timeout *utils.Timeout) error {
 		err = b.consensus.timeoutHandler(b.blockChainReader, timeout)
 		if err != nil {
 			if _, ok := err.(*utils.ErrIncomingMessageRoundNotEqualCurrentRound); ok {
-				log.Warn("timeout round not equal", "error", err)
+				log.Debug("timeout round not equal", "error", err)
 				return err
 			}
 			log.Error("handle BFT Timeout", "error", err)

e323970fd8c6e8e407bb8d25f372b6063f9f5aeb
Liam Lai <liam.icheng.lai@gmail.com>
2022-08-05 12:12:30 +0530

    change to 5 sec

--- params/config.go
+++ params/config.go
@@ -58,8 +58,8 @@ var (
 		CertThreshold:        6,
 		TimeoutSyncThreshold: 5,
 		TimeoutPeriod:        10,
-		WaitPeriod:           2,
-		MinePeriod:           2,
+		WaitPeriod:           5,
+		MinePeriod:           5,
 	}
 
 	// XDPoSChain mainnet config

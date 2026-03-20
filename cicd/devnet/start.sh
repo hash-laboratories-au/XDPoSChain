68b260f1f0b283b414a1d9afe8886a7a03fdd4bc
Wang Gerui <wgr523@gmail.com>
2026-03-20 23:19:26 +0800

    chore: add fastsync args to devnet start.sh

--- cicd/devnet/start.sh
+++ cicd/devnet/start.sh
@@ -95,6 +95,23 @@ else
   gc_mode=$GC_MODE
 fi
 
+fastsync_args=""
+if test -n "$FASTSYNC_PIVOT_NUMBER"
+then
+  echo "FASTSYNC_PIVOT_NUMBER found, set to $FASTSYNC_PIVOT_NUMBER"
+  fastsync_args="${fastsync_args} --fastsyncpivotnumber ${FASTSYNC_PIVOT_NUMBER}"
+fi
+if test -n "$FASTSYNC_PIVOT_HASH"
+then
+  echo "FASTSYNC_PIVOT_HASH found, set to $FASTSYNC_PIVOT_HASH"
+  fastsync_args="${fastsync_args} --fastsyncpivothash ${FASTSYNC_PIVOT_HASH}"
+fi
+if test -n "$FASTSYNC_PIVOT_ROOT"
+then
+  echo "FASTSYNC_PIVOT_ROOT found, set to $FASTSYNC_PIVOT_ROOT"
+  fastsync_args="${fastsync_args} --fastsyncpivotroot ${FASTSYNC_PIVOT_ROOT}"
+fi
+
 netstats="${NODE_NAME}-${wallet}-${instance_ip}:xinfin_xdpos_hybrid_network_stats@devnetstats.hashlabs.apothem.network:1999"
 
 
@@ -117,4 +134,5 @@ XDC --ethstats ${netstats} \
 --debugdatadir /work/xdcchain \
 --store-reward \
 --ws --ws-addr=0.0.0.0 --ws-port $ws_port \
+${fastsync_args} \
 --ws-origins "*" 2>&1 >>/work/xdcchain/xdc.log | tee -a /work/xdcchain/xdc.log
